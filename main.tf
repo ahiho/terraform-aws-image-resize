0b99b0687e7e7b9ea4858fa441a0c603108bbc42
duckhue01 <duckhue01.tech@gmail.com>
2024-01-05 11:22:06 +0700

    feat: add aws provider version constaint

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 5.0.0"
+      version               = ">= 5.31.0"
       configuration_aliases = [aws.origin_region, aws.distribution_region]
 
     }
